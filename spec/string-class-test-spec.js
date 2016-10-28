f514c66c9d6990e3cd0925cb1ef56dbd1593a73a
Femi Oladeji <femidotexe@gmail.com>
2016-10-28 15:58:52 +0100

    Test: Add edge case for hasVowels() method

--- spec/string-class-test-spec.js
+++ spec/string-class-test-spec.js
@@ -1,11 +1,15 @@
 'use strict';
 
-describe('hasVowels method', function() {
+describe('Test for hasVowels() method', function() {
   it('should return false if "dfhgjl".hasVowels() was executed', function() {
     expect('dfhgjl'.hasVowels()).toBe(false);
   });
 
   it('should return true if "word".hasVowels() was executed', function() {
     expect('word'.hasVowels()).toBe(true);
+  });
+
+  it('should return false if "78371038--- ".hasVowels() was executed', function() {
+    expect('78371038--- '.hasVowels()).toBe(false);
   });
 });
