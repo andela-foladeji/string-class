e78ef76b7cc7dc721f4e32c2e844c23cddcdb719
Femi Oladeji <femidotexe@gmail.com>
2016-11-03 21:24:49 +0100

    Test: Change test to mocha

--- test/string-class-test.js
+++ test/string-class-test.js
@@ -1,187 +1,192 @@
 'use strict';
+const assert = require('assert');
+require('../src/string-class');
 
 describe('Test for hasVowels() method', () => {
   it('should return false if dfhgjl.hasVowels() was executed', () => {
-    expect('dfhgjl'.hasVowels()).toBe(false);
+    assert.equal('dfhgjl'.hasVowels(), false);
   });
 
   it('should return true if word.hasVowels() was executed', () => {
-    expect('word'.hasVowels()).toBe(true);
+    assert.equal('word'.hasVowels(), true);
   });
 
   it('should return true for cAp.hasVowels() was executed', () => {
-    expect('cAp'.hasVowels()).toBe(true);
+    assert.equal('cAp'.hasVowels(), true);
   });
 
   it('should return false if "78371038--- ".hasVowels() \
     was executed', () => {
-      expect('78371038--- '.hasVowels()).toBe(false);
-    });
+    assert.equal('78371038--- '.hasVowels(), false);
+  });
 });
 
 describe('Test for toUpper() method', () => {
   it('should return ANDELA if anDelA.toUpper() was executed', () => {
-    expect('anDelA'.toUpper()).toBe('ANDELA');
+    assert.equal('anDelA'.toUpper(), 'ANDELA');
   });
 
   it('should return LOWER CASE for "lower case".toUpper() \
     was executed', () => {
-      expect('lower case'.toUpper()).toBe('LOWER CASE');
+    assert.equal('lower case'.toUpper(), 'LOWER CASE');
   });
 
   it('should return 123-456 for 123-456.toUpper()', () => {
-    expect('123-456'.toUpper()).toBe('123-456');
+    assert.equal('123-456'.toUpper(), '123-456');
   });
 });
 
 describe('Test for toLower() method', () => {
   it('should return "this is" for "THIS IS".toLower()', () => {
-    expect('THIS IS'.toLower()).toBe('this is');
+    assert.equal('THIS IS'.toLower(), 'this is');
   });
 
   it('should return "already lower" for "already lower".toLower()', () => {
-    expect('already lower'.toLower()).toBe('already lower');
+    assert.equal('already lower'.toLower(), 'already lower');
   });
 
   it('should return "1,000,000." for "1,000,000.".toLower()', () => {
-    expect('1,000,000.'.toLower()).toBe('1,000,000.');
+    assert.equal('1,000,000.'.toLower(), '1,000,000.');
   });
 });
 
 describe('Test for ucFirst method', () => {
   it('should return FirSt for "firSt".ucFirst()', () => {
-    expect('first'.ucFirst()).toBe('First');
+    assert.equal('firSt'.ucFirst(), 'FirSt');
   });
 
   it('should return Andela "Andela".ucFirst()', () => {
-    expect('Andela'.ucFirst()).toBe('Andela');
+    assert.equal('Andela'.ucFirst(), 'Andela');
   });
 
   it('should return 12word "12word".ucFirst()', () => {
-    expect('12word'.ucFirst()).toBe('12word');
+    assert.equal('12word'.ucFirst(), '12word');
   });
 });
 
 describe('Test for isQuestion method', () => {
   it('should return true for "Is this a question?".isQuestion()', () => {
-    expect('Is this a question?'.isQuestion()).toBe(true);
+    assert.equal('Is this a question?'.isQuestion(), true);
   });
 
   it('should return false for "Is this a ? question".isQuestion()', () => {
-    expect('Is this a ? question'.isQuestion()).toBe(false);
+    assert.equal('Is this a ? question'.isQuestion(), false);
   });
 
   it('should return true for "What?      ".isQuestion()', () => {
-    expect('What?      '.isQuestion()).toBe(true);
+    assert.equal('What?      '.isQuestion(), true);
   });
 });
 
 describe('Test for words method', () => {
   it('should return an array of the list of words ["It", "is", "working"]\
     for "It is working".words()', () => {
-      expect('It is working'.words()).toEqual(['It', 'is', 'working']);
-    });
+    assert.deepEqual('It is working'.words(), ['It', 'is', 'working']);
+  });
 
   it('should return [] for "     ".words()', () => {
-    expect('     '.words()).toEqual([]);
+    assert.deepEqual('     '.words(), []);
   });
 
-  it('should return [I, have, $1,000,000,000] for I have $1,000,000,000 ".words()', () => {
-    expect('I have $1,000,000,000'.words()).toEqual(['I', 'have', '1000000000']);
-  });
+  it('should return [I, have, $1,000,000,000] for \
+    I have $1,000,000,000 ".words()', () => {
+      assert.deepEqual('I have $1,000,000,000'.words(),
+        ['I', 'have', '1000000000']);
+    });
 });
 
 describe('Test for wordCount method', () => {
   it('should return 3 for "It is working".wordCount()', () => {
-    expect('It is working'.wordCount()).toBe(3);
+    assert.equal('It is working'.wordCount(), 3);
   });
 
   it('should return 5 for "Wierdly    spaced     text   \
-    contains 87938".wordCount()',() => {
-      expect('Wierdly    spaced     text   contains 87938'.wordCount()).toBe(5);
-    });
+    contains 87938".wordCount()', () => {
+    assert.equal('Wierdly    spaced     text   contains 87938'.wordCount(),
+      5);
+  });
 });
 
 describe('Test for toCurrency method', () => {
   it('should return 11,111.11 for 11111.11.toCurrency()', () => {
-    expect('11111.11'.toCurrency()).toBe('11,111.11');
+    assert.equal('11111.11'.toCurrency(), '11,111.11');
   });
 
   it('should return 1,000,000 for 1000000.toCurrency()', () => {
-    expect('1000000'.toCurrency()).toBe('1,000,000');
+    assert.equal('1000000'.toCurrency(), '1,000,000');
   });
 });
 
 describe('Test for fromCurrency method', () => {
   it('should return 11111.11 for 11,111.11.fromCurrency()', () => {
-    expect('11,111.11'.fromCurrency()).toBe('11111.11');
+    assert.equal('11,111.11'.fromCurrency(), '11111.11');
   });
 
   it('should return 1000000 for 1,000,000.fromCurrency()', () => {
-    expect('1,000,000'.fromCurrency()).toBe('1000000');
+    assert.equal('1,000,000'.fromCurrency(), '1000000');
   });
 });
 
 describe('Test for inverseCase method', () => {
   it('should return mR. bEN for "Mr. Ben".inverseCase()', () => {
-    expect('Mr. Ben'.inverseCase()).toBe('mR. bEN');
+    assert.equal('Mr. Ben'.inverseCase(), 'mR. bEN');
   });
 
   it('should return aNonymOuS for AnONYMoUs.inverseCase()', () => {
-    expect('AnONYMoUs'.inverseCase()).toBe('aNonymOuS');
+    assert.equal('AnONYMoUs'.inverseCase(), 'aNonymOuS');
   });
 });
 
 describe('Test for alternatingCase method', () => {
   it('should return oNoMaToPoEiA for "Onomatopoeia".alternatingCase()', () => {
-    expect('Onomatopoeia'.alternatingCase()).toBe('oNoMaToPoEiA');
+    assert.equal('Onomatopoeia'.alternatingCase(), 'oNoMaToPoEiA');
   });
 
   it('should return iNtErEsTiNg for iNTErestIng.alternatingCase()', () => {
-    expect('iNTErestIng'.alternatingCase()).toBe('iNtErEsTiNg');
+    assert.equal('iNTErestIng'.alternatingCase(), 'iNtErEsTiNg');
   });
 });
 
 describe('Test for getMiddle method', () => {
   it('should return ea for read.getMiddle()', () => {
-    expect('read'.getMiddle()).toBe('ea');
+    assert.equal('read'.getMiddle(), 'ea');
   });
 
   it('should return a for reads.getMiddle()', () => {
-    expect('reads'.getMiddle()).toBe('a');
+    assert.equal('reads'.getMiddle(), 'a');
   });
 });
 
 describe('Test for numberWords method', () => {
   it('should return three two five for 325.numberWords()', () => {
-    expect('325'.numberWords()).toBe('three two five');
+    assert.equal('325'.numberWords(), 'three two five');
   });
 
   it('should return one zero zero zero for 1,000.numberWords()', () => {
-    expect('1,000'.numberWords()).toBe('one, zero zero zero');
+    assert.equal('1,000'.numberWords(), 'one, zero zero zero');
   });
 });
 
 describe('Test for isDigit method', () => {
   it('should return true for 3', () => {
-    expect('3'.isDigit()).toBe(true);
+    assert.equal('3'.isDigit(), true);
   });
 
   it('should return return false for 34', () => {
-    expect('45'.isDigit()).toBe(false);
+    assert.equal('45'.isDigit(), false);
   });
 });
 
 describe('Test for doubleCheck method', () => {
   it('should return true for aagjglkdk.doubleCheck()', () => {
-    expect('aagjglkdk'.doubleCheck()).toBe(true);
+    assert.equal('aagjglkdk'.doubleCheck(), true);
   });
 
   it('should return true for spaced  twice', () => {
-    expect('spaced  twice'.doubleCheck()).toBe(true);
+    assert.equal('spaced  twice'.doubleCheck(), true);
   });
 
   it('should return false for nothing', () => {
-    expect('nothing'.doubleCheck()).toBe(false);
+    assert.equal('nothing'.doubleCheck(), false);
   });
 });
